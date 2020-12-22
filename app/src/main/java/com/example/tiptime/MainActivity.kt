d96b441dd0b5293d1aa82a1b4cda5ebf69de2bd9
Abdulrafay <abdulrafayn001@gmail.com>
2020-12-22 17:47:53 +0500

    Calculate Tip and dsiplay on TextView

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.example.tiptime
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.calculateBtn.setOnClickListener{
+            calculateTip()
+        }
+    }
+
+    @SuppressLint("StringFormatInvalid")
+    private fun calculateTip() {
+        val cost=binding.costOfService.text.toString().toDoubleOrNull()
+        if(cost==null)
+        {
+            binding.tipRsult.text=""
+            return
+        }
+        val selectedId=binding.options.checkedRadioButtonId
+        val tipPercentage=when(selectedId){
+            R.id.option_twenty_percent ->0.2
+            R.id.option_eighteen_percent ->0.18
+            else -> 0.15
+        }
+        var tip=tipPercentage*cost
+        if(binding.roundUpId.isChecked){
+            tip=kotlin.math.ceil(tip)
+        }
+        displayTip(tip)
+
+    }
+
+    private fun displayTip(tip: Double) {
+        val formattedTip=NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipRsult.text = "Tip Amount $formattedTip"
     }
 }
